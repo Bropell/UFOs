368e03ed2a10de102dc190d0c45e7a2f723fbfd6
Brandon Ropell <brandon.ropell@gmail.com>
2022-08-11 21:33:59 -0400

    Added final exercises from class

---
+++ Exercises/onChange/Solved/static/js/app_starter.js
@@ -0,0 +1,26 @@
+// grab references to the input element and the output div
+let text = d3.select("#text");
+let output = d3.select(".output");
+
+// Function to reverse a string
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+
+// Function to handle input change
+function handleChange(event) {
+  // grab the value of the input field
+  let inputText = d3.event.target.value;
+
+  // clear the existing output
+  output.html("");
+
+  // reverse the input string
+  let reversedInput = reverseString(inputText);
+
+  // Set the output text to the reversed input string
+  output.text(reversedInput);
+}
+
+// Attach an event to detect changes to the input field.
+text.on("change", handleChange);
